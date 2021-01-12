1a10ffe948b6999369c456d66cf3129a6781c832
Savaivanenko <savaivanenko@gmail>
2021-01-12 19:10:09 +0200

    12.01  19:00

---
+++ src/components/phonebook/Filter.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+ const Filter =({value, onChange}) =>(
+   <label>
+     Поиск контакта:
+     <input type="text" value={value} onChange={onChange}/>
+   </label>
+ )
+ export default Filter;
